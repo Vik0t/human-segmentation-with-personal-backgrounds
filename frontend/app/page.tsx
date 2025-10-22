b5f2fb1d79a7090a88a2393a8e0abad18544cbdb
Serge46b <serg46b@gmail.com>
2025-10-23 05:58:39 +0700

    Basic web UI done

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,14 +5,12 @@ import BGSelector from "@/components/BGSelector"
 import { useRef, useState, useTransition, useEffect } from "react"
 
 export default function Home() {
-  const [isPreviewHidden, setIsPreviewHidden] = useState(false)
   const [isConnected, setIsConnected] = useState(false)
   const [isPending, startTransition] = useTransition()
 
   const srcVideoRef = useRef<HTMLVideoElement>(null)
   const videoRef = useRef<HTMLVideoElement>(null)
   const pcRef = useRef<RTCPeerConnection | null>(null)
-  const localStreamRef = useRef<MediaStream | null>(null)
   const isIceCompleteRef = useRef(false)
 
   const checkState = () => {
@@ -45,34 +43,13 @@ export default function Home() {
     <div className="h-full w-full flex flex-col items-center justify-center gap-4 bg-gray-100 p-8">
       <h1 className="m-0 text-3xl font-bold text-center text-gray-800">WebRTC Webcam</h1>
       <div className="h-full bg-gray-200 rounded-lg overflow-hidden relative">
-        
-          <video
-            ref={srcVideoRef}
-            autoPlay
-            playsInline
-            onClick={() => {
-              setIsPreviewHidden(true)
-            }}
-            className={`absolute top-5 left-5 object-cover border-2 border-gray-300 rounded-lg ${
-              isConnected && !isPreviewHidden ? "block" : "hidden"
-            }`}
-          />
-        
-        {isPreviewHidden && (
-          <button
-            onClick={() => {
-              setIsPreviewHidden(false)
-            }}
-            className="absolute top-5 left-5 w-15 h-15 bg-gray-500 opacity-50 text-white text-center flex items-center justify-center font-bold rounded-lg"
-          >
-            Show
-          </button>
-        )}
-        {!isConnected && (
-          <div className="absolute top-0 left-0 w-full h-full bg-gray-500 opacity-50 text-white text-center flex items-center justify-center text-5xl font-bold">
-            Press 'start' to start the stream
-          </div>
-        )}
+        <video
+          ref={srcVideoRef}
+          autoPlay
+          playsInline
+          className={`absolute top-5 left-5 w-30% h-30% object-cover border-2 border-gray-300 rounded-lg ${isConnected ? "block" : "hidden"}`}
+        />
+        {!isConnected && <div className="absolute top-0 left-0 w-full h-full bg-gray-500 opacity-50 text-white text-center flex items-center justify-center text-5xl font-bold">Press 'start' to start the stream</div>}
         <video ref={videoRef} autoPlay playsInline className="w-full h-full object-cover" />
       </div>
       <div className="flex flex-row gap-4 justify-center h-10">
@@ -83,19 +60,16 @@ export default function Home() {
               pcRef.current!.close()
               videoRef.current!.srcObject = null
               srcVideoRef.current!.srcObject = null
-              localStreamRef.current?.getTracks().forEach((track) => {
-                track.stop()
-              })
               setIsConnected(false)
               return
             }
             startTransition(async () => {
-              localStreamRef.current = await navigator.mediaDevices.getUserMedia({
+              const localStream = await navigator.mediaDevices.getUserMedia({
                 video: true,
                 audio: false,
-              })
-              srcVideoRef.current!.srcObject = localStreamRef.current
-              localStreamRef.current.getTracks().forEach((track) => {
+              });
+              srcVideoRef.current!.srcObject = localStream
+              localStream.getTracks().forEach((track) => {
                 pcRef.current!.addTrack(track)
               })
               const offerData = await pcRef.current!.createOffer()
